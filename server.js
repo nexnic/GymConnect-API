bbd5e610ff456c8ff280aa92b7f557abe2caa5bf
Kent Erik Hole <kent.erik.hole@outlook.com>
2026-03-25 15:55:09 +0100

    fikses som bugs

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ const dotenv = require('dotenv');
 // Route to the config.env file
 dotenv.config({ path: './secret.env' });
 
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const app = require('./app');
+
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log('DB connection succesfull!');
 });
 
 const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
