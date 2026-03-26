1cf37dd03820bf1c8893d07662b222b532d694c9
Kent Erik Hole <kent.erik.hole@outlook.com>
2026-03-26 13:55:55 +0100

    remove bug from the import to const

--- models/SettingsModal.js
+++ models/SettingsModal.js
@@ -1,11 +1,14 @@
-const mongoose from ('mongoose');
+const mongoose = require('mongoose');
 
-const SettingsSchema = new mongoose.Scheam({
-  gymName: { type: String, required: true },
-  address: { type: String, required: true },
-  monthlyPrice: Number,
-  quarterlyPrice: Number,
-  annualPrice: Number,
-}, { timestamps: true });
+const SettingsSchema = new mongoose.Scheam(
+  {
+    gymName: { type: String, required: true },
+    address: { type: String, required: true },
+    monthlyPrice: Number,
+    quarterlyPrice: Number,
+    annualPrice: Number,
+  },
+  { timestamps: true },
+);
 
-export default mongoose.model('Settings', SettingsSchema)
+export default mongoose.model('Settings', SettingsSchema);
