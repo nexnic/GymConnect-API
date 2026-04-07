66d370110375d274589a923b6d2f330b4f89f82a
Kent Erik Hole <kent.erik.hole@outlook.com>
2026-04-07 12:13:21 +0200

    done  with userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,3 +8,8 @@ const router = express.Router();
 
 // Get user profile
 router.get('/', getUserProfile);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+export default router;
